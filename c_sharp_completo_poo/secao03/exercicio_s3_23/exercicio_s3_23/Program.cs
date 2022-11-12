2e26e23502a8ff0fb3c1799133d5f6ae00d743af
Muniz Neto <muniznetto@outlook.com>
2022-11-12 19:07:32 -0300

    exercisio proposto aula 23

---
+++ c_sharp_completo_poo/secao03/exercicio_s3_23/exercicio_s3_23/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ Ler valor e  mostrarna tela 
+ */
+
+using System.Globalization;
+
+Console.WriteLine("Entre com seu nome completo: ");
+string nome_completo = Console.ReadLine();
+
+Console.WriteLine("Quantos quartos tem na sua casa? ");
+int quartos = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Entre com o preco de um produto: ");
+double preco_produto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Entre com seu último nome, idade e altura: ");
+string[] vetor = Console.ReadLine().Split(' ');
+
+Console.WriteLine(nome_completo);
+Console.WriteLine(quartos);
+Console.WriteLine(preco_produto.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine(vetor[0]);
+Console.WriteLine(vetor[1]);
+Console.WriteLine(vetor[2]);
+
+
